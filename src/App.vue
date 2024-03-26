4a48c6882dd173b23a9918ec02606940bf6de511
mthdht <mthdht@gmail.com>
2024-03-26 12:54:13 +0100

    made preview for uploaded image

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="test h-[50rem] p-4">
-    <MediaLibrary></MediaLibrary>
+    <MediaLibrary v-model="images"></MediaLibrary>
+    {{ images }}
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import MediaLibrary from './components/MediaLibrary.vue'
+
+
+const items = [
+  {'src': 'ok', 'alt': 'alt text 1'},
+  {'src': 'ok', 'alt': 'alt text 2'},
+  {'src': 'ok', 'alt': 'alt text 3'},
+  {'src': 'ok', 'alt': 'alt text 4'},
+  {'src': 'ok', 'alt': 'alt text 5'},
+]
+
+const images = ref(items)
 </script>
 
 
--- src/components/MediaLibrary.vue
+++ src/components/MediaLibrary.vue
@@ -4,20 +4,42 @@
         <aside class="gallery w-96 border-2 p-4 flex flex-col">
             <h3 class="font-semibold text-slate- mb-8 text-2xl">{{ title || 'Medias' }}</h3>
             <div class="image-gallery grid grid-cols-2 gap-4 max-h-full overflow-auto">
-                <button class="aspect-square bg-emerald-500 text-white flex justify-center items-center">
+                <input type="file" name="image" id="" class="hidden" ref="fileInput" @change="handleFileInput">
+                <button class="aspect-square bg-emerald-500 text-white flex justify-center items-center" @click="triggerFileInput">
                     <PlusIcon class="size-12"></PlusIcon>
                 </button>
 
-                <div v-for="n in 8" class="bg-red-300 aspect-square">ok</div>
+                <div v-for="image in model" class="bg-red-300 aspect-square">
+                    <img :src="image.src" :alt="image.alt" srcset="">
+                </div>
             </div>
         </aside>
     </section>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { PlusIcon } from '@heroicons/vue/24/solid'
 
 const props = defineProps({
     title: String
 })
+
+const model = defineModel()
+
+const fileInput = ref()
+
+const triggerFileInput = () => {
+    console.log('clicked')
+    fileInput.value.click()
+}
+
+const handleFileInput = (event) => {
+    console.log('image upload: ' + URL.createObjectURL(event.target.files[0]))
+    model.value.push({
+        'src': URL.createObjectURL(event.target.files[0]),
+        'alt': 'ok',
+        'file': event.target.files[0]
+    })
+}
 </script>
