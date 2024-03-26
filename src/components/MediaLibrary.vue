34c36110b9e79ed230a63f488e823e1752720c87
mthdht <mthdht@gmail.com>
2024-03-26 09:44:25 +0100

    made html structure

---
+++ src/components/MediaLibrary.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="media-library">
+        <div class="preview"></div>
+        <aside class="gallery"></aside>
+    </section>
+</template>
+
+<style scoped>
+.media-library {
+    display: flex;
+    gap: 12px;
+    height: 100%
+}
+
+.preview, .gallery {
+    border: 1px solid blue
+}
+
+.preview {
+    flex-grow: 1
+}
+.gallery {
+    width: 384px
+}
+
+* {
+    box-sizing: border-box;
+}
+</style>
