3383eb48e976310c56beab22ace3726ce722fe8d
mthdht <mthdht@gmail.com>
2024-04-02 13:57:52 +0200

    start layout for image preview

--- src/components/MediaLibrary.vue
+++ src/components/MediaLibrary.vue
@@ -1,15 +1,23 @@
 <template>
     <section class="media-library flex gap-2 h-full">
-        <div class="preview grow border-2 p-8"></div>
+        <div class="preview grow border-2 p-8 flex flex-col items-center gap-8 h-full">
+            <div class="images grow p-8 bg-slate-100 self-stretch">    
+                <img :src="currentPreview?.src" :alt="currentPreview?.alt" srcset="" class="object-contain size-full">
+            </div>
+            <div class="controls flex gap-4 mb-8">
+                <div v-for="n in 10">ok</div>
+
+            </div>
+        </div>
         <aside class="gallery w-96 border-2 p-4 flex flex-col">
             <h3 class="font-semibold text-slate- mb-8 text-2xl">{{ title || 'Medias' }}</h3>
-            <div class="image-gallery grid grid-cols-2 gap-4 max-h-full overflow-auto mb-8">
+            <div class="image-gallery grid grid-cols-2 gap-4 max-h-full overflow-auto mb-8 grow">
                 <input type="file" name="image" id="" class="hidden" ref="fileInput" @change="handleFileInput">
                 <button class="aspect-square bg-emerald-500 text-white flex justify-center items-center" @click="triggerFileInput">
                     <PlusIcon class="size-12"></PlusIcon>
                 </button>
 
-                <div v-for="(image, index) in model" class="bg-red-300 aspect-square group relative" :key="index">
+                <div v-for="(image, index) in model" class="bg-red-300 aspect-square group relative" :key="index" @click="preview(index)">
                     <div class="absolute size-full group-hover:bg-slate-100/70">
                         <div class="hidden group-hover:flex justify-end p-2 gap-2 text-slate-700">
                             <button @click="handleDelete(index)"><TrashIcon class="size-5 hover:text-slate-800"></TrashIcon></button>
@@ -18,24 +26,31 @@
                     <img :src="image.src" :alt="image.alt" srcset="" class="h-full w-full object-cover">
                 </div>
             </div>
-            <button class="px-3 py-2 rounded bg-emerald-500 text-white">save</button>
+            <button class="px-3 py-2 rounded bg-emerald-500 text-white" @click="saveFiles">save</button>
         </aside>
     </section>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { PlusIcon, TrashIcon } from '@heroicons/vue/24/solid'
 
 const props = defineProps({
     title: String
 })
 
-const emit = defineEmits(['addFile', 'deleteFile'])
+const emit = defineEmits(['addFile', 'deleteFile', 'saveFiles'])
 
 const model = defineModel()
 
 const fileInput = ref()
+
+const currentPreview = ref(model.value[0])
+
+
+const preview = (index) => {
+    currentPreview.value = model.value[index] ?? model.value[0]
+}
 
 const triggerFileInput = () => {
     fileInput.value.click()
@@ -55,6 +70,15 @@ const handleDelete = (index) => {
     emit('deleteFile', model.value[index])
 
     model.value.splice(index, 1)
+}
 
+const saveFiles = () => {
+    emit('saveFiles')
 }
 </script>
+
+<style scoped>
+    .images {
+        height: calc(100% - 150px)
+    }
+</style>
