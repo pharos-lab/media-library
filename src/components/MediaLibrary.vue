1d2607f0d4a5df4bd1a37d6247e9d438ac4bf8af
mthdht <mthdht@gmail.com>
2024-04-16 10:00:48 +0200

    add undo icon

--- src/components/MediaLibrary.vue
+++ src/components/MediaLibrary.vue
@@ -15,7 +15,12 @@
                 </div>
             </div>
 
-            <div class="controls flex gap-4 justify-center bg-slate-100 p-8">
+            <div class="controls flex gap-4 justify-center items-center bg-slate-100 p-8">
+                <button title="undo">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="size-5">
+                        <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z"/>
+                    </svg>
+                </button>
                 <button title="crop image" @click="open('crop')">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="size-5">
                         <path d="M128 32c0-17.7-14.3-32-32-32S64 14.3 64 32V64H32C14.3 64 0 78.3 0 96s14.3 32 32 32H64V384c0 35.3 28.7 64 64 64H352V384H128V32zM384 480c0 17.7 14.3 32 32 32s32-14.3 32-32V448h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H448l0-256c0-35.3-28.7-64-64-64L160 64v64l224 0 0 352z"/>
